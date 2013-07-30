414a2d88dcc1667a4c2d0394bea7fa02d28492d4
Derek Petersen <derek@stumbleupon.com>
2013-07-29 17:31:40 -0700

    Add support for CoffeeScript

--- tasks/grunt-cp.js
+++ tasks/grunt-cp.js
@@ -1,6 +1,6 @@
 /**
- * grunt-commonjs
- * https://github.com/avos/grunt-commonjs
+ * grunt-commonjs-coffee
+ * https://github.com/tuxracer/grunt-commonjs-coffee
  *
  * Copyright (c) 2013 Team Delicious, AVOS Systems Inc.
  * Licensed under the MIT license.
@@ -27,9 +27,16 @@ module.exports = function(grunt) {
     this.files.forEach(function(file) {
       return file.src.map(function(filepath) {
         var definePath = (filepath.replace(/\.\w+$/, '')),
-            original = grunt.file.read(path.join(file.cwd, filepath));
-        return grunt.file.write(file.dest + filepath, 'window.require.define({"' + definePath + '": function(exports, require, module) {\n' + original + '}});\n\n');
+            original = grunt.file.read(path.join(file.cwd, filepath)),
+            isCoffeeScript = filepath.slice(-7) === ".coffee";
+
+        if (isCoffeeScript) {
+          original = original.replace(/(\r\n|\n|\r)/gm,"\n  ");
+          return grunt.file.write(file.dest + filepath, 'window.require.define "' + definePath + '": (exports, require, module) ->\n  ' + original + '\n');
+        } else {
+          return grunt.file.write(file.dest + filepath, 'window.require.define({"' + definePath + '": function(exports, require, module) {\n' + original + '}});\n\n');
+        }
       });
     });
   });
-}
+};
